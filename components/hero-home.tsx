f58151ce108edf3363193ab07b71cc262703df4c
sadik alike <sadikalike7@gmail.com>
2026-02-09 17:58:37 +0530

    UI updates and smooth scroll setup a

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -10,26 +10,45 @@ export default function HeroHome() {
           {/* Section header */}
           <div className="pb-12 text-center md:pb-20">
             <h1
-              className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,var(--color-gray-200),var(--color-indigo-200),var(--color-gray-50),var(--color-indigo-300),var(--color-gray-200))] bg-[length:200%_auto] bg-clip-text pb-5 font-nacelle text-4xl font-semibold text-transparent md:text-5xl"
-              data-aos="fade-up"
-            >
-              Hi sadik, how are yoiu
-            </h1>
+  className="animate-[gradient_6s_linear_infinite]
+  bg-[linear-gradient(to_right,var(--color-gray-200),#22c55e,var(--color-gray-50),#16a34a,var(--color-gray-200))]
+  bg-[length:200%_auto]
+  bg-clip-text
+  pb-5
+  font-nacelle
+  text-4xl font-semibold
+  text-transparent
+  md:text-5xl"
+  data-aos="fade-up"
+>
+  Hi Sadik, how are you
+</h1>
+
             <div className="mx-auto max-w-3xl">
-              <p
-                className="mb-8 text-xl text-indigo-200/65"
-                data-aos="fade-up"
-                data-aos-delay={200}
-              >
-                Our landing page template works on all devices, so you only have
-                to set it up once, and get beautiful results forever.
-              </p>
+             <p
+  className="mb-8 text-xl text-white"
+  data-aos="fade-up"
+  data-aos-delay={200}
+>
+  Our landing page template works on all devices, so you only have
+  to set it up once, and get beautiful results forever.
+</p>
+
+
               <div className="mx-auto max-w-xs sm:flex sm:max-w-none sm:justify-center">
                 <div data-aos="fade-up" data-aos-delay={400}>
-                  <a
-                    className="btn group mb-4 w-full bg-linear-to-t from-indigo-600 to-indigo-500 bg-[length:100%_100%] bg-[bottom] text-white shadow-[inset_0px_1px_0px_0px_--theme(--color-white/.16)] hover:bg-[length:100%_150%] sm:mb-0 sm:w-auto"
-                    href="#0"
-                  >
+                <a
+  className="btn group mb-4 w-full
+  bg-gradient-to-t from-slate-950 via-emerald-900 to-emerald-700
+  bg-[length:100%_100%] bg-[bottom]
+  text-white
+  shadow-[0_0_60px_rgba(16,185,129,0.35),inset_0px_1px_0px_rgba(255,255,255,0.12)]
+  hover:bg-[length:100%_160%]
+  hover:shadow-[0_0_90px_rgba(16,185,129,0.55)]
+  sm:mb-0 sm:w-auto"
+  href="#0"
+>
+
                     <span className="relative inline-flex items-center">
                       Start Building
                       <span className="ml-1 tracking-normal text-white/50 transition-transform group-hover:translate-x-0.5">
